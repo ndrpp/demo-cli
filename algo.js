a544f46a211792342ac5ee6059a758b8216a4ca2
ndrpp <popandrei230@yahoo.com>
2026-02-09 10:53:13 +0200

    fix: update algo with correct dataset path

--- algo.js
+++ algo.js
@@ -1,30 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// List directory contents recursively for debugging
-function listDir(dir, depth = 0) {
-  const indent = '  '.repeat(depth);
-  if (!fs.existsSync(dir)) {
-    console.log(`${indent}${dir} [does not exist]`);
-    return;
-  }
-  const stat = fs.statSync(dir);
-  if (!stat.isDirectory()) {
-    const size = stat.size;
-    console.log(`${indent}${dir} [file, ${size} bytes]`);
-    return;
-  }
-  console.log(`${indent}${dir}/`);
-  const entries = fs.readdirSync(dir);
-  for (const entry of entries) {
-    listDir(path.join(dir, entry), depth + 1);
-  }
-}
-
-console.log('=== /data/ directory listing ===');
-listDir('/data');
-console.log('================================');
-
 // Find and read the data file from Ocean compute mount point
 function findDataFile() {
   const inputDir = '/data/inputs/0';
@@ -36,6 +12,7 @@ function findDataFile() {
   }
 
   const possiblePaths = [
+    '/data/inputs/data.json',
     '/data/inputs/0/data.json',
     '/data/inputs/0/0',
   ];
@@ -54,19 +31,6 @@ function findDataFile() {
       const firstFile = path.join(inputDir, files[0]);
       console.log(`Reading first file found: ${firstFile}`);
       return JSON.parse(fs.readFileSync(firstFile, 'utf8'));
-    }
-  }
-
-  // Also check /data/inputs directly for any files
-  const inputsDir = '/data/inputs';
-  if (fs.existsSync(inputsDir) && fs.statSync(inputsDir).isDirectory()) {
-    const entries = fs.readdirSync(inputsDir);
-    for (const entry of entries) {
-      const entryPath = path.join(inputsDir, entry);
-      if (fs.statSync(entryPath).isFile()) {
-        console.log(`Reading from /data/inputs/: ${entryPath}`);
-        return JSON.parse(fs.readFileSync(entryPath, 'utf8'));
-      }
     }
   }
 
